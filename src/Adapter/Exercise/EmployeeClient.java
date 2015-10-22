f2619cb0304f10d4ca8e7dd734c79bdd693a817c
Andrew Speed <aspeed@localhost.localdomain>
2015-10-22 23:54:57 +0100

    Exercise and demo for Adapter pattern

---
+++ src/Adapter/Exercise/EmployeeClient.java
@@ -0,0 +1,27 @@
+package Adapter.Exercise;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EmployeeClient {
+
+    public List<Employee> getEmployeeList() {
+        List<Employee> employees = new ArrayList<>();
+
+        Employee employeeFromDb = new EmployeeDb("1234", "John", "Wick", "John@Wick.com");
+
+        employees.add(employeeFromDb);
+
+        //Employee employeeFromLdap = new EmployeeLdap("Chewie", "Solo", "Han", "han@solo.com");
+
+        EmployeeLdap employeeFromLdap = new EmployeeLdap("Chewie", "Solo", "Han", "han@solo.com");
+
+        EmployeeCSV employeeFromCSV = new EmployeeCSV("567,Sherlock,Holmes,sherlock@holmes.com");
+
+        // adapt EmployeeLdap and EmployeeCSV classes to Employee
+        employees.add(new EmployeeAdapterLdap(employeeFromLdap));
+        employees.add(new EmployeeAdapterCSV(employeeFromCSV));
+
+        return employees;
+    }
+}
